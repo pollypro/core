4dc84ff94e0a8221185d84ce45a5ddee6328a3c7
Ivan Stetsenko <i.stetsenko1@gmail.com>
2020-08-20 23:40:29 +0300

    call getConnection right before the app is assembled.

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,19 @@
 import { config as configEnv } from 'dotenv-safe';
 configEnv();
 
+// imports from vendors
 import express from 'express';
 import cors from 'cors';
-import { runApp } from './utils/server';
 
+// imports from routes
 import PingRouter from './routes/ping';
+
+// imports from utils
+import { runApp } from './utils/server';
+import { getConnection } from './utils/mongodb';
+
+// Initial MongoDB connection establishment
+getConnection();
 
 const app = express();
 
