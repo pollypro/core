ddf2cc8cc6c5a809734a0beec54b050987020297
Ivan Stetsenko <i.stetsenko1@gmail.com>
2020-08-20 23:26:04 +0300

    add mongodb utils

---
+++ src/utils/mongodb.ts
@@ -0,0 +1,19 @@
+import { MongoClient } from 'mongodb';
+
+let connection: MongoClient = null;
+
+export const getConnection = async (): Promise<MongoClient> => {
+  if (connection instanceof MongoClient) {
+    return connection;
+  }
+
+  const mongoClient = new MongoClient(process.env.MONGODB_URL, { useNewUrlParser: true });
+
+  try {
+    connection = await mongoClient.connect();
+  } catch (e) {
+    // TODO: catch it
+    console.log(e);
+  }
+  return connection;
+};
