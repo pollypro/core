974103285c352ead0d17b38e2f3d72736e4553b5
Ivan Stetsenko <i.stetsenko1@gmail.com>
2020-08-23 01:59:49 +0300

    add IssueAuthToken command

---
+++ src/commands/users/IssueAuthToken.ts
@@ -0,0 +1,15 @@
+import { issueToken } from '../../utils/jwt';
+import AuthenticateUser from './AuthenticateUser';
+import FindUserByEmail from './FindUserByEmail';
+import { IUser } from '../../types/user';
+
+export default class IssueAuthToken {
+
+  public static readonly dependsOn = [FindUserByEmail, AuthenticateUser];
+
+  async execute(context: { user: IUser }) {
+    const token = issueToken(context.user);
+    return { ...context, token };
+  }
+
+}
