6cfb3a5e637642e5a0656e1386706ed551ea0f20
Ivan Stetsenko <i.stetsenko1@gmail.com>
2020-08-22 20:57:54 +0300

    add jwt utils with issueToken fn

---
+++ src/utils/jwt.ts
@@ -0,0 +1,9 @@
+import jwt from 'jsonwebtoken';
+import { JWT_SECRET, JWT_ISSUER } from '../constants/jwt';
+import { IUser } from '../types/user';
+
+export const issueToken = (user: IUser) => jwt.sign(
+  { user },
+  JWT_SECRET,
+  { expiresIn: 86400, issuer: JWT_ISSUER }
+);
