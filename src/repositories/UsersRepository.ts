7ad16fc1f6652079597fe2db34b5e0acd0833227
Ivan Stetsenko <i.stetsenko1@gmail.com>
2020-08-22 19:49:33 +0300

    add initial UsersRepository

---
+++ src/repositories/UsersRepository.ts
@@ -0,0 +1,33 @@
+import { getConnection } from '../utils/mongodb';
+
+interface User {
+  firstName: string;
+  lastName: string;
+  company: string;
+  permissions: string[];
+  phone: string;
+  email: string;
+  password: string;
+  status: 'activated' | 'deactivated';
+}
+
+export default class UsersRepository {
+
+  static async getCollection() {
+    const connection = await getConnection();
+    return connection.db().collection('users');
+  }
+
+  static async insertOne(document: User) {
+    const collection = await UsersRepository.getCollection();
+
+    return await collection
+      .insertOne(document);
+  }
+
+  static async wipe() {
+    const collection = await UsersRepository.getCollection();
+
+    return await collection.deleteMany({});
+  }
+}
