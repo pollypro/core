f8faf9f1fc24f207d524791159458134384304e2
Ivan Stetsenko <i.stetsenko1@gmail.com>
2020-08-23 01:59:39 +0300

    add AuthenticateUser command

---
+++ src/commands/users/AuthenticateUser.ts
@@ -0,0 +1,23 @@
+import { compareSync } from 'bcrypt';
+import Unauthorized from '../../errors/Unauthorized';
+import FindUserByEmail from './FindUserByEmail';
+import { IUser } from '../../types/user';
+
+type Params = {
+  email: string;
+  password: string
+}
+
+export default class AuthenticateUser {
+
+  public static readonly dependsOn = [FindUserByEmail];
+
+  async execute(context: { user: IUser }, { password }: Params) {
+    if (!compareSync(password, context.user.password)) {
+      throw new Unauthorized();
+    }
+
+    return context;
+  }
+
+}
