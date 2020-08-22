b7b9b0a704b83507b91322baf89f7fbb8e923cde
Ivan Stetsenko <i.stetsenko1@gmail.com>
2020-08-22 20:22:14 +0300

    explicitly establish and close the mongodb connection

--- scripts/seed/seed.ts
+++ scripts/seed/seed.ts
@@ -1,12 +1,17 @@
 import { config as configEnv } from 'dotenv-safe';
 configEnv();
 
+import { getConnection } from '../../src/utils/mongodb';
 import { seedUsers } from './users';
 import { wipe } from './wipe';
 
 const seed = async () => {
+  const connection = await getConnection();
+
   await wipe();
   await seedUsers();
+
+  await connection.close();
 };
 
 seed();
