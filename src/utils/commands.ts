9e0eec671a4e295da33871c26a208e09ebe819f3
Ivan Stetsenko <i.stetsenko1@gmail.com>
2020-08-23 02:00:05 +0300

    add commands utils

---
+++ src/utils/commands.ts
@@ -0,0 +1,12 @@
+export const runCommand = async (Command: any, context: Record<string, any>, params = {}) => {
+  const queue = []
+    .concat(Array.isArray(Command.dependsOn) ? Command.dependsOn : [], Command)
+    .map((CommandClass) => new CommandClass());
+
+  let contextAcc = context;
+  for (const CommandInstance of queue) {
+    contextAcc = await CommandInstance.execute(contextAcc, params);
+  }
+
+  return contextAcc;
+};
