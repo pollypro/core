668df32aa0cf1ec1ff708e7b0dd1e233d5dcf9e5
Ivan Stetsenko <i.stetsenko1@gmail.com>
2020-08-23 00:57:15 +0300

    add DocumentNotFound error

---
+++ src/repositories/errors/DocumentNotFound.ts
@@ -0,0 +1,9 @@
+export default class DocumentNotFound extends Error {
+
+  constructor(message?: string) {
+    super(message);
+
+    Object.setPrototypeOf(this, DocumentNotFound.prototype);
+  }
+
+}
