649ea17953e3afd4b64e988acff2e2562735cd93
Ivan Stetsenko <i.stetsenko1@gmail.com>
2020-08-23 00:57:54 +0300

    add FindUserByEmail command

---
+++ src/commands/users/FindUserByEmail.ts
@@ -0,0 +1,10 @@
+import { UsersRepository } from '../../repositories';
+
+export default class FindUserByEmail {
+
+  async execute(context: Record<string, any>, { email }: { email: string }) {
+    const user = await UsersRepository.findByEmail(email);
+    return { ...context, user };
+  }
+
+}
