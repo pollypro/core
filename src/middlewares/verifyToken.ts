5a0bacfb6304640e2331b13f854d3bc3c9145dc5
Ivan Stetsenko <i.stetsenko1@gmail.com>
2020-08-30 02:08:08 +0300

    add verifyToken middleware

---
+++ src/middlewares/verifyToken.ts
@@ -0,0 +1,33 @@
+// imports from vendors
+import { Request, Response, NextFunction } from 'express';
+import httpContext from 'express-http-context';
+import jwt from 'jsonwebtoken';
+
+// imports from constants
+import { JWT_ISSUER, JWT_SECRET } from '../constants/jwt';
+
+export default (request: Request, response: Response, next: NextFunction) => {
+  const sendForbidden = () => response.status(403).send();
+
+  const authorization = request.headers.authorization;
+
+  if (!authorization) {
+    return sendForbidden();
+  }
+
+  const [type, token] = authorization.split(' ');
+
+  if (!type || !token || type !== 'Bearer') {
+    sendForbidden();
+  }
+
+  jwt.verify(token, JWT_SECRET, { issuer: JWT_ISSUER }, (error, decoded: { user: any }) => {
+    if (error) {
+      sendForbidden();
+    }
+
+    httpContext.set('currentUser', decoded.user);
+
+    next();
+  })
+};
