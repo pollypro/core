3df6a94d906c6e3984abfdbee849f872dc341ea3
Ivan Stetsenko <i.stetsenko1@gmail.com>
2020-08-30 02:17:01 +0300

    add /list-companies route

--- src/routes/companies.ts
+++ src/routes/companies.ts
@@ -1,6 +1,7 @@
 import { Request, Response, Router } from 'express';
 import { runCommand } from '../utils/commands';
 import CreateCompany from '../commands/companies/CreateCompany';
+import ListCompanies from '../commands/companies/ListCompanies';
 import verifyToken from '../middlewares/verifyToken';
 
 const CompaniesRouter = Router();
@@ -9,6 +10,15 @@ CompaniesRouter.post('/create-company', verifyToken, async (request: Request, re
   try {
     const context = await runCommand(CreateCompany, {}, request.body);
     response.status(200).json({ company: context.company });
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+CompaniesRouter.post('/list-companies', verifyToken, async (request: Request, response: Response) => {
+  try {
+    const context = await runCommand(ListCompanies, {});
+    response.status(200).json(context.companies);
   } catch (e) {
     console.log(e);
   }
