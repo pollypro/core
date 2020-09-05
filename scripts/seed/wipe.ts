e3bdfe2a4b318c1f010ab4f6209edbd54b5b3e0d
Ivan Stetsenko <i.stetsenko1@gmail.com>
2020-09-06 01:30:25 +0300

    drop CompaniesRepository, log the wipe time

--- scripts/seed/wipe.ts
+++ scripts/seed/wipe.ts
@@ -1,6 +1,9 @@
 import { UsersRepository } from '../../src/repositories';
+import { CompaniesRepository } from '../../src/repositories';
 
 export const wipe = async () => {
+  console.time('DB wipe');
+  await CompaniesRepository.drop();
   await UsersRepository.drop();
-  console.log('Users dropped');
+  console.timeEnd('DB wipe');
 };
