f8069a7bab4d1f7c2cd7e85076eda99b43f9d35d
Ivan Stetsenko <i.stetsenko1@gmail.com>
2020-09-06 02:04:34 +0300

    use UserObject instead of IUser

--- src/commands/users/IssueAuthToken.ts
+++ src/commands/users/IssueAuthToken.ts
@@ -1,11 +1,11 @@
 import { issueToken } from '../../utils/jwt';
 import AuthenticateUser from './AuthenticateUser';
-import { IUser } from '../../types/user';
+import { UserObject } from '../../repositories/mappers/users';
 
 export default class IssueAuthToken {
   public static readonly dependsOn = [AuthenticateUser];
 
-  async execute(context: { user: IUser }) {
+  async execute(context: { user: UserObject }) {
     const token = issueToken(context.user);
     return { ...context, token };
   }
