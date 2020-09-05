f5d28df950ba7065ed0953e8d8e2e52e816e287f
Ivan Stetsenko <i.stetsenko1@gmail.com>
2020-09-05 22:34:06 +0300

    authenticate: send 401 if DocumentNotFound

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -2,6 +2,7 @@ import { Request, Response, Router } from 'express';
 import { runCommand } from '../utils/commands';
 import IssueAuthToken from '../commands/users/IssueAuthToken';
 import { httpCodeByError } from '../utils/http';
+import { DocumentNotFound } from '../repositories/errors';
 
 const AuthRouter = Router();
 
@@ -10,6 +11,10 @@ AuthRouter.post('/authenticate', async (request: Request, response: Response) =>
     const context = await runCommand(IssueAuthToken, {}, request.body);
     response.status(200).json({ token: context.token });
   } catch (error) {
+    if (error instanceof DocumentNotFound) {
+      return response.sendStatus(401);
+    }
+
     const status = httpCodeByError(error);
     response.sendStatus(status);
   }
