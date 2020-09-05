2cba2b3b9b12f434cbabb2f03d156df500841387
Ivan Stetsenko <i.stetsenko1@gmail.com>
2020-09-06 02:04:08 +0300

    execute context arg: remove typing since there's no user

--- src/commands/users/AuthenticateUser.ts
+++ src/commands/users/AuthenticateUser.ts
@@ -1,9 +1,8 @@
 import Unauthorized from '../../errors/Unauthorized';
-import { IUser } from '../../types/user';
 import UsersRepository from '../../repositories/UsersRepository';
 
 export default class AuthenticateUser {
-  async execute(context: { user: IUser }, params: { email: string; password: string }) {
+  async execute(context: Record<string, any>, params: { email: string; password: string }) {
     const user = await UsersRepository.authenticate(params.email, params.password);
 
     if (!user) {
