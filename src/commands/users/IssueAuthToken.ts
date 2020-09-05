046d74f17662e1f2f4f7bffec4744568d233ca9a
Ivan Stetsenko <i.stetsenko1@gmail.com>
2020-09-05 23:03:43 +0300

    remove FindUserByEmail dep

--- src/commands/users/IssueAuthToken.ts
+++ src/commands/users/IssueAuthToken.ts
@@ -1,10 +1,9 @@
 import { issueToken } from '../../utils/jwt';
 import AuthenticateUser from './AuthenticateUser';
-import FindUserByEmail from './FindUserByEmail';
 import { IUser } from '../../types/user';
 
 export default class IssueAuthToken {
-  public static readonly dependsOn = [FindUserByEmail, AuthenticateUser];
+  public static readonly dependsOn = [AuthenticateUser];
 
   async execute(context: { user: IUser }) {
     const token = issueToken(context.user);
