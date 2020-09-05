5f7cb6179609c0fb47349005fad7431026d8e9e8
Ivan Stetsenko <i.stetsenko1@gmail.com>
2020-09-06 02:47:25 +0300

    remove seed/users, run directly in seed.ts file

--- scripts/seed/seed.ts
+++ scripts/seed/seed.ts
@@ -1,15 +1,24 @@
 import { config as configEnv } from 'dotenv-safe';
 configEnv();
 
+import { ObjectId } from 'mongodb';
 import { getConnection } from '../../src/utils/mongodb';
-import { seedUsers } from './users';
+import { CompaniesRepository, UsersRepository } from '../../src/repositories';
 import { wipe } from './wipe';
+import { superUser, superCompany } from './data';
 
 const seed = async () => {
   const connection = await getConnection();
 
   await wipe();
-  await seedUsers();
+
+  console.time('Seeding');
+  const company = await CompaniesRepository.insertOne(superCompany);
+  await UsersRepository.insertOne({
+    ...superUser,
+    companyId: new ObjectId(company.id),
+  });
+  console.timeEnd('Seeding');
 
   await connection.close();
 };
