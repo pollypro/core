8f218b6281e8238db8d97704854a61c5eaa0ce21
Ivan Stetsenko <i.stetsenko1@gmail.com>
2020-09-06 14:39:28 +0300

    add CompanyObject type, map createdAt, updatedAt

--- src/repositories/mappers/companies.ts
+++ src/repositories/mappers/companies.ts
@@ -1,6 +1,15 @@
 import { CompanySchema } from '../schemas/company';
 
-export const mapCompany = (document: CompanySchema) => ({
+export type CompanyObject = {
+  id: string;
+  name: string;
+  createdAt: Date;
+  updatedAt?: Date;
+};
+
+export const mapCompany = (document: CompanySchema): CompanyObject => ({
   id: document._id.toString(),
   name: document.name,
+  createdAt: document.createdAt,
+  updatedAt: document.updatedAt,
 });
