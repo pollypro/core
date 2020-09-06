2debe4c18348be23cc92585a332720ad6333bea4
Ivan Stetsenko <i.stetsenko1@gmail.com>
2020-09-07 00:46:47 +0300

    add customRules

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,8 +1,13 @@
 import Unauthorized from '../errors/Unauthorized';
+import DocumentNotFound from '../errors/DocumentNotFound';
 
-export const httpCodeByError = (error: any) => {
+export const httpCodeByError = (error: any, customRules: Map<any, number> = new Map()) => {
   if (error instanceof Unauthorized) {
-    return 401;
+    return customRules.get(Unauthorized) ?? 401;
+  }
+
+  if (error instanceof DocumentNotFound) {
+    return customRules.get(DocumentNotFound) ?? 404;
   }
 
   return 500;
