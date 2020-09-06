c9f264ec9fd37ab013a2a5235816ab6943fb8c99
Ivan Stetsenko <i.stetsenko1@gmail.com>
2020-09-06 22:15:05 +0300

    execute: accept pagination params

--- src/commands/companies/ListCompanies.ts
+++ src/commands/companies/ListCompanies.ts
@@ -1,11 +1,15 @@
 import { CompaniesRepository } from '../../repositories';
 import CurrentUser from '../users/CurrentUser';
+import { PaginationParams } from '../../repositories/utils/pagination';
 
 export default class ListCompanies {
   public static readonly dependsOn = [CurrentUser];
 
-  async execute(context: Record<string, any>) {
-    const companies = await CompaniesRepository.list();
+  async execute(
+    context: Record<string, any>,
+    { params }: { params: PaginationParams } = { params: {} },
+  ) {
+    const companies = await CompaniesRepository.list({ params });
     return { ...context, companies };
   }
 }
