4f559b70d94d452c234d9c69e88b41a0689fd791
Ivan Stetsenko <i.stetsenko1@gmail.com>
2020-09-11 02:00:46 +0300

    add /delete-user endpoint

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -2,6 +2,7 @@ import { Request, Response, Router } from 'express';
 import { runCommand } from '../utils/commands';
 import CurrentUser from '../commands/users/CurrentUser';
 import ListAdmins from '../commands/users/ListAdmins';
+import DeleteUser from '../commands/users/DeleteUser';
 import verifyToken from '../middlewares/verifyToken';
 import { httpCodeByError } from '../utils/http';
 
@@ -16,6 +17,16 @@ UsersRouter.post('/list-admins', verifyToken, async (request: Request, response:
   try {
     const context = await runCommand(ListAdmins, {}, request.body);
     response.status(200).json(context.admins);
+  } catch (error) {
+    const status = httpCodeByError(error);
+    response.sendStatus(status);
+  }
+});
+
+UsersRouter.post('/delete-user', verifyToken, async (request: Request, response: Response) => {
+  try {
+    const context = await runCommand(DeleteUser, {}, request.body);
+    response.status(204).json(context.admins);
   } catch (error) {
     const status = httpCodeByError(error);
     response.sendStatus(status);
