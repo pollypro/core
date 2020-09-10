451de0aea624c517815107042f46ecf5041c05a3
Ivan Stetsenko <i.stetsenko1@gmail.com>
2020-09-11 00:55:57 +0300

    add listAdmins method

--- src/repositories/UsersRepository.ts
+++ src/repositories/UsersRepository.ts
@@ -3,6 +3,8 @@ import { compare } from 'bcrypt';
 import { getConnection, collectionExists } from '../utils/mongodb';
 import { UserSchema } from './schemas/User';
 import { mapUser } from './mappers/users';
+import { getMongoPagination, PaginationParams } from './utils/pagination';
+import { mapCompany } from './mappers/companies';
 
 export type NewUserDocument = {
   firstName: string;
@@ -34,6 +36,23 @@ export default class UsersRepository {
     try {
       const result = await collection.insertOne({ ...document, createdAt: new Date() });
       return mapUser(result.ops[0]);
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  static async listAdmins({ params }: { params: PaginationParams } = { params: {} }) {
+    const collection = await UsersRepository.getCollection();
+    const pagination = getMongoPagination(params);
+
+    try {
+      const result = await collection
+        .find({ isAdmin: true })
+        .sort(pagination.sort)
+        .skip(pagination.skip)
+        .limit(pagination.limit)
+        .toArray();
+      return result.map(mapUser);
     } catch (e) {
       throw e;
     }
