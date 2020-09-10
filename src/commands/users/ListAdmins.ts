49034962767fb118a93f41ded5e23466a5711f90
Ivan Stetsenko <i.stetsenko1@gmail.com>
2020-09-11 00:56:25 +0300

    add ListAdmins command

---
+++ src/commands/users/ListAdmins.ts
@@ -0,0 +1,15 @@
+import { UsersRepository } from '../../repositories';
+import CurrentUser from '../users/CurrentUser';
+import { PaginationParams } from '../../repositories/utils/pagination';
+
+export default class ListAdmins {
+  public static readonly dependsOn = [CurrentUser];
+
+  async execute(
+    context: Record<string, any>,
+    { params }: { params: PaginationParams } = { params: {} },
+  ) {
+    const companies = await UsersRepository.listAdmins({ params });
+    return { ...context, companies };
+  }
+}
