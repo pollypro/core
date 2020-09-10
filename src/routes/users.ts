77862e9d9b20a9c08981c7ed26be74ad89a875fb
Ivan Stetsenko <i.stetsenko1@gmail.com>
2020-09-11 00:56:57 +0300

    add /list-admins endpoint

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,6 +1,7 @@
 import { Request, Response, Router } from 'express';
 import { runCommand } from '../utils/commands';
 import CurrentUser from '../commands/users/CurrentUser';
+import ListAdmins from '../commands/users/ListAdmins';
 import verifyToken from '../middlewares/verifyToken';
 
 const UsersRouter = Router();
@@ -8,6 +9,15 @@ const UsersRouter = Router();
 UsersRouter.post('/get-self', verifyToken, async (request: Request, response: Response) => {
   const context = await runCommand(CurrentUser, {});
   response.status(200).json(context.currentUser);
+});
+
+UsersRouter.post('/list-admins', verifyToken, async (request: Request, response: Response) => {
+  try {
+    const context = await runCommand(ListAdmins, {}, request.body);
+    response.status(200).json(context.companies);
+  } catch (e) {
+    console.log(e);
+  }
 });
 
 export default UsersRouter;
