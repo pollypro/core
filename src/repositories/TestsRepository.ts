7440c9f339c76a35ef59d585c76772e003674ba6
Ivan Stetsenko <i.stetsenko1@gmail.com>
2020-09-16 11:16:12 +0300

    add initial TestsRepository with insertOne, drop methods

---
+++ src/repositories/TestsRepository.ts
@@ -0,0 +1,32 @@
+import { getConnection, collectionExists } from '../utils/mongodb';
+import { mapTest } from './mappers/tests';
+import { TestSchema } from './schemas/test';
+
+type NewTestDocument = {
+  name: string;
+};
+
+export default class TestsRepository {
+  static async getCollection() {
+    const connection = await getConnection();
+    return connection.db().collection<TestSchema>('tests');
+  }
+
+  static async insertOne(document: NewTestDocument) {
+    const collection = await TestsRepository.getCollection();
+
+    try {
+      const result = await collection.insertOne({ ...document, createdAt: new Date() });
+      return mapTest(result.ops[0]);
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  static async drop() {
+    if (await collectionExists('tests')) {
+      const collection = await TestsRepository.getCollection();
+      await collection.drop();
+    }
+  }
+}
