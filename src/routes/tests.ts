465d56d67d4abd2f5ff405caee568b8b21faf1d7
Ivan Stetsenko <i.stetsenko1@gmail.com>
2020-09-16 12:52:19 +0300

    add /delete-test endpoint

--- src/routes/tests.ts
+++ src/routes/tests.ts
@@ -2,6 +2,7 @@ import { Request, Response, Router } from 'express';
 import { runCommand } from '../utils/commands';
 import CreateTest from '../commands/tests/CreateTest';
 import UpdateTest from '../commands/tests/UpdateTest';
+import DeleteTest from '../commands/tests/DeleteTest';
 import verifyToken from '../middlewares/verifyToken';
 import { httpCodeByError } from '../utils/http';
 
@@ -20,6 +21,16 @@ TestsRouter.post('/create-test', verifyToken, async (request: Request, response:
 TestsRouter.post('/update-test', verifyToken, async (request: Request, response: Response) => {
   try {
     await runCommand(UpdateTest, {}, request.body);
+    response.status(204).send();
+  } catch (error) {
+    const status = httpCodeByError(error);
+    response.sendStatus(status);
+  }
+});
+
+TestsRouter.post('/delete-test', verifyToken, async (request: Request, response: Response) => {
+  try {
+    await runCommand(DeleteTest, {}, request.body);
     response.status(204).send();
   } catch (error) {
     const status = httpCodeByError(error);
