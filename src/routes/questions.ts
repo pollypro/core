eaf684e0e8a142a0043b75b6e2bad55260345165
Ivan Stetsenko <i.stetsenko1@gmail.com>
2020-09-16 18:54:04 +0300

    add /update-question endpoint

--- src/routes/questions.ts
+++ src/routes/questions.ts
@@ -3,6 +3,7 @@ import verifyToken from '../middlewares/verifyToken';
 import { httpCodeByError } from '../utils/http';
 import { runCommand } from '../utils/commands';
 import CreateQuestion from '../commands/questions/CreateQuestion';
+import UpdateQuestion from '../commands/questions/UpdateQuestion';
 
 const QuestionsRouter = Router();
 
@@ -13,6 +14,20 @@ QuestionsRouter.post(
     try {
       const context = await runCommand(CreateQuestion, {}, request.body);
       response.status(200).json(context.question);
+    } catch (error) {
+      const status = httpCodeByError(error);
+      response.sendStatus(status);
+    }
+  },
+);
+
+QuestionsRouter.post(
+  '/update-question',
+  verifyToken,
+  async (request: Request, response: Response) => {
+    try {
+      await runCommand(UpdateQuestion, {}, request.body);
+      response.status(204).send();
     } catch (error) {
       const status = httpCodeByError(error);
       response.sendStatus(status);
