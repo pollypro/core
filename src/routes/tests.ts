a118c8aab341a69f94d7498ec3e744e885d4c94f
Ivan Stetsenko <i.stetsenko1@gmail.com>
2020-09-16 12:47:24 +0300

    add /update-test endpoint

--- src/routes/tests.ts
+++ src/routes/tests.ts
@@ -1,6 +1,7 @@
 import { Request, Response, Router } from 'express';
 import { runCommand } from '../utils/commands';
 import CreateTest from '../commands/tests/CreateTest';
+import UpdateTest from '../commands/tests/UpdateTest';
 import verifyToken from '../middlewares/verifyToken';
 import { httpCodeByError } from '../utils/http';
 
@@ -10,6 +11,16 @@ TestsRouter.post('/create-test', verifyToken, async (request: Request, response:
   try {
     const context = await runCommand(CreateTest, {}, request.body);
     response.status(200).json(context.test);
+  } catch (error) {
+    const status = httpCodeByError(error);
+    response.sendStatus(status);
+  }
+});
+
+TestsRouter.post('/update-test', verifyToken, async (request: Request, response: Response) => {
+  try {
+    await runCommand(UpdateTest, {}, request.body);
+    response.status(204).send();
   } catch (error) {
     const status = httpCodeByError(error);
     response.sendStatus(status);
