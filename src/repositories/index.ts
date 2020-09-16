ce395b589bff54265d64a887b92b7b565c137c16
Ivan Stetsenko <i.stetsenko1@gmail.com>
2020-09-16 11:17:31 +0300

    add TestsRepository export

--- src/repositories/index.ts
+++ src/repositories/index.ts
@@ -1,2 +1,3 @@
-export { default as UsersRepository } from './UsersRepository';
 export { default as CompaniesRepository } from './CompaniesRepository';
+export { default as TestsRepository } from './TestsRepository';
+export { default as UsersRepository } from './UsersRepository';
