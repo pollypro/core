870fdcde832cbf744b56c1434d4fca2e600b465e
Ivan Stetsenko <i.stetsenko1@gmail.com>
2020-09-21 00:02:20 +0300

    add LinkUsersToService command

---
+++ src/commands/services/LinkUsersToService.ts
@@ -0,0 +1,20 @@
+import { ObjectId } from 'mongodb';
+import { ServicesRepository } from '../../repositories';
+import CurrentUser from '../users/CurrentUser';
+import FindServiceById from './FindServiceById';
+import { ServiceObject } from '../../repositories/mappers/services';
+
+export default class LinkUsersToService {
+  public static readonly dependsOn = [CurrentUser, FindServiceById];
+
+  async execute(
+    context: { service: ServiceObject },
+    { serviceId, users, companies }: { serviceId: string; users: string[]; companies: string[] },
+  ) {
+    await ServicesRepository.updateById(serviceId, {
+      users: [...context.service.users, ...users].map((id) => new ObjectId(id)),
+      companies: [...context.service.companies, ...companies].map((id) => new ObjectId(id)),
+    });
+    return context;
+  }
+}
