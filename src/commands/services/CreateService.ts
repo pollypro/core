b2e9de811917bea46645c9c9efffe23fafbfa567
Ivan Stetsenko <i.stetsenko1@gmail.com>
2020-09-20 23:30:49 +0300

    Params: add companies, users

--- src/commands/services/CreateService.ts
+++ src/commands/services/CreateService.ts
@@ -1,9 +1,12 @@
+import { ObjectId } from 'mongodb';
 import { ServicesRepository } from '../../repositories';
 import CurrentUser from '../users/CurrentUser';
 
 type Params = {
   service: {
     name: string;
+    companies?: string[];
+    users?: string[];
   };
 };
 
@@ -11,7 +14,11 @@ export default class CreateService {
   public static readonly dependsOn = [CurrentUser];
 
   async execute(context: Record<string, any>, params: Params) {
-    const service = await ServicesRepository.insertOne(params.service);
+    const service = await ServicesRepository.insertOne({
+      ...params.service,
+      companies: (params.service.companies || []).map((id) => new ObjectId(id)),
+      users: (params.service.users || []).map((id) => new ObjectId(id)),
+    });
     return { ...context, service };
   }
 }
