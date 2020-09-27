f2984838db28005642a05ed6614640698073997f
Ivan Stetsenko <i.stetsenko1@gmail.com>
2020-09-27 03:55:20 +0300

    UnlinkUsersFromService: add params validation

--- src/commands/services/UnlinkUsersFromService.ts
+++ src/commands/services/UnlinkUsersFromService.ts
@@ -1,29 +1,46 @@
+import * as yup from 'yup';
 import { ObjectId } from 'mongodb';
 import { ServicesRepository } from '../../repositories';
 import CurrentUser from '../users/CurrentUser';
 import FindServiceById from './FindServiceById';
 import { ServiceObject } from '../../repositories/mappers/services';
+import ValidationFailed from '../../errors/ValidationFailed';
+
+type Params = {
+  serviceId: string;
+  users?: string[];
+  companies?: string[];
+};
+
+const ParamsSchema = yup.object({
+  serviceId: yup.string().required(),
+  users: yup.array().of(yup.string()).optional().default([]),
+  companies: yup.array().of(yup.string()).optional().default([]),
+});
 
 export default class UnlinkUsersFromService {
   public static readonly dependsOn = [CurrentUser, FindServiceById];
 
-  async execute(
-    context: { service: ServiceObject },
-    {
-      serviceId,
-      users = [],
-      companies = [],
-    }: { serviceId: string; users: string[]; companies: string[] },
-  ) {
-    await ServicesRepository.updateById(serviceId, {
+  async execute(context: { service: ServiceObject }, params: Params) {
+    const validParams = await UnlinkUsersFromService.validateParams(params);
+
+    await ServicesRepository.updateById(validParams.serviceId, {
       users: context.service.users
-        .filter((id) => !users.includes(id))
+        .filter((id) => !validParams.users.includes(id))
         .map((id) => new ObjectId(id)),
       companies: context.service.companies
-        .filter((id) => !companies.includes(id))
+        .filter((id) => !validParams.companies.includes(id))
         .map((id) => new ObjectId(id)),
     });
 
     return context;
+  }
+
+  static async validateParams(params: Params): Promise<Params> | never {
+    try {
+      return await ParamsSchema.validate(params, { stripUnknown: true });
+    } catch (ValidationError) {
+      throw new ValidationFailed();
+    }
   }
 }
