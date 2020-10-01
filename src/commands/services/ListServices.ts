816c5f972c503947cbcac076d432d8d7d5615df1
Ivan Stetsenko <i.stetsenko1@gmail.com>
2020-10-01 03:33:25 +0300

    ListServices: add checkPermissions static method, return services for user if not currentUser.isAdmin

--- src/commands/services/ListServices.ts
+++ src/commands/services/ListServices.ts
@@ -1,15 +1,31 @@
+import { ObjectId } from 'mongodb';
 import { ServicesRepository } from '../../repositories';
 import CurrentUser from '../users/CurrentUser';
 import { PaginationParams } from '../../repositories/utils/pagination';
+import { UserObject } from '../../repositories/mappers/users';
+
+type Context = {
+  currentUser: UserObject;
+};
 
 export default class ListServices {
   public static readonly dependsOn = [CurrentUser];
 
   async execute(
-    context: Record<string, any>,
+    context: Context,
     { params }: { params: PaginationParams } = { params: {} },
   ) {
-    const services = await ServicesRepository.list({ params });
+    const { query } = await ListServices.checkPermissions(context);
+
+    const services = await ServicesRepository.list({ query, params });
     return { ...context, services };
+  }
+
+  static async checkPermissions(context: Context) {
+    if (!context.currentUser.isAdmin) {
+      return { query: { users: new ObjectId(context.currentUser.id) } };
+    }
+
+    return { query: {} };
   }
 }
