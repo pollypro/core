50cadad946c8ad24e7d24478d2a3e3707e32f9b1
Ivan Stetsenko <i.stetsenko1@gmail.com>
2020-10-02 03:07:27 +0300

    CreateTest: add params validation

--- src/commands/tests/CreateTest.ts
+++ src/commands/tests/CreateTest.ts
@@ -1,6 +1,13 @@
 import { ObjectId } from 'mongodb';
+import * as yup from 'yup';
 import { TestsRepository } from '../../repositories';
 import CurrentUser from '../users/CurrentUser';
+import { UserObject } from '../../repositories/mappers/users';
+import ValidationFailed from '../../errors/ValidationFailed';
+
+type Context = {
+  currentUser: UserObject;
+};
 
 type Params = {
   test: {
@@ -9,14 +16,33 @@ type Params = {
   };
 };
 
+const ParamsSchema = yup.object({
+  test: yup
+    .object({
+      name: yup.string().required(),
+      users: yup.array().of(yup.string()).optional().default([]), // TODO: ObjectId.isValid otherwise 500
+    })
+    .required(),
+});
+
 export default class CreateTest {
   public static readonly dependsOn = [CurrentUser];
 
-  async execute(context: Record<string, any>, params: Params) {
+  async execute(context: Context, params: Params) {
+    const validParams = await CreateTest.validateParams(params);
+
     const test = await TestsRepository.insertOne({
-      ...params.test,
-      users: (params.test.users || []).map((id) => new ObjectId(id)),
+      ...validParams.test,
+      users: validParams.test.users.map((id) => new ObjectId(id)),
     });
     return { ...context, test };
+  }
+
+  static async validateParams(params: Params): Promise<Params> | never {
+    try {
+      return await ParamsSchema.validate(params, { stripUnknown: true });
+    } catch (ValidationError) {
+      throw new ValidationFailed();
+    }
   }
 }
